4f48138b5ea24fdd100752b1f59f8fd8e1ef4595
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-23 11:29:17 -0400

    Configure app to accept JSON / Image data. Tested data values in auth routes.

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ const config = require("./config/app");
 const router = require("./router");
 
 const app = express();
-app.use(router);
 const PORT = config.appPort;
+
+app.use(express.urlencoded({ extended: true })); // Images
+app.use(express.json()); // JSON Data from client
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
