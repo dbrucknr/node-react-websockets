ec57966b8b63fd317d7f31833f26dd1e4b0456da
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-23 09:17:32 -0400

    Set base express config.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/home", (req, res) => {
+  return res.send("Home");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
