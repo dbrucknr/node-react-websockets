7e0cf890dfad397c37e22d47e94cfac236fa390e
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-24 13:32:28 -0400

    add cors to server, axios to react; complete login component.

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,8 +1,25 @@
 import loginImage from "../../assets/images/login.svg";
 import "./auth.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://127.0.0.1:8000/login", { email: email, password: password })
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((err) => {
+        console.error("error", err);
+      });
+  };
+
   return (
     <div id="auth-container">
       <div id="auth-card">
@@ -13,12 +30,24 @@ export const Login = () => {
           <div id="form-section">
             <h2>Welcome Back</h2>
 
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="input-field mb-1">
-                <input placeholder="Email" />
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  required="required"
+                  type="text"
+                  placeholder="Email"
+                />
               </div>
               <div className="input-field mb-2">
-                <input placeholder="Password" />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required="required"
+                  type="password"
+                  placeholder="Password"
+                />
               </div>
               <button>Login</button>
             </form>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const config = require("./config/app");
 const router = require("./router");
+const cors = require("cors");
 
 const app = express();
 const PORT = config.appPort;
 
 app.use(express.urlencoded({ extended: true })); // Images
 app.use(express.json()); // JSON Data from client
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, () => {
