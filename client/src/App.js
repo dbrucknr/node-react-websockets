f04240bd103af4267843374d5f6e88cb5a197882
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-25 14:11:38 -0400

    Add fontawesome to project.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,33 @@ import { ProtectedRoute } from "./components/protectedRoute";
 
 import "./App.scss";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSmile, faImage } from "@fortawesome/free-regular-svg-icons";
+import {
+  faSpinner,
+  faEllipsisV,
+  faUserPlus,
+  faSignOutAlt,
+  faTrash,
+  faCaretDown,
+  faUpload,
+  faTimes,
+  faBell,
+} from "@fortawesome/free-solid-svg-icons";
+library.add(
+  faSmile,
+  faImage,
+  faSpinner,
+  faEllipsisV,
+  faUserPlus,
+  faSignOutAlt,
+  faTrash,
+  faCaretDown,
+  faUpload,
+  faTimes,
+  faBell
+);
+
 const NoPageFound = () => <div>404 Page Not Found</div>;
 
 function App() {
--- client/src/components/messenger/components/navbar.js
+++ client/src/components/messenger/components/navbar.js
@@ -1,5 +1,6 @@
 import "./navbar.scss";
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Navbar = () => {
   const user = useSelector((state) => state.authReducer.user);
@@ -11,6 +12,7 @@ export const Navbar = () => {
         <img width="40" height="40" src={user.avatar} alt="avatar" />
         <p>
           {user.firstName} {user.lastName}
+          <FontAwesomeIcon icon="caret-down" className="fa-icon" />
         </p>
       </div>
     </div>
