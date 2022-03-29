caffd0c58d97033250dc6a614a8cee3f81197f5c
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-29 17:05:39 -0400

    begin applying styles to friend component.

--- client/src/components/messenger/components/friend/friend.js
+++ client/src/components/messenger/components/friend/friend.js
@@ -1,9 +1,33 @@
+import "./Friend.scss";
+import { useSelector } from "react-redux";
+import { userStatus } from "../../../../utils/helpers";
+
 export const Friend = ({ thread }) => {
+  const currentThread = useSelector(
+    (state) => state.messengerReducer.currentThread
+  );
+  const isThreadOpened = () => (currentThread.id === thread.id ? "opened" : "");
+  const lastMessage = () => {
+    if (thread.Messages.length === 0) {
+      return "";
+    }
+    const message = thread.Messages[thread.Messages.length - 1];
+    return message.type === "image" ? "Image Uploaded" : message.message;
+  };
   return (
-    <div>
-      <p>
-        {thread.Users[0].firstName} {thread.Users[0].lastName}
-      </p>
+    <div className={`friend-list ${isThreadOpened()}`}>
+      <div>
+        <img width="40" height="40" src={thread.Users[0].avatar} alt="Avatar" />
+        <div className="friend-info">
+          <h4 className="m-0">
+            {thread.Users[0].firstName} {thread.Users[0].lastName}
+          </h4>
+          <h5 className="m-0">{lastMessage()}</h5>
+        </div>
+      </div>
+      <div className="friend-status">
+        <span className={`online-status ${userStatus(thread.Users[0])}`}></span>
+      </div>
     </div>
   );
 };
