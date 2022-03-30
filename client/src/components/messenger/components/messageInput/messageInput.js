fa0c9400c105ef38a059d9ec7b68cf36cfe5ee80
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-30 12:02:23 -0400

    WIP - setting messageInput component structure and logic.

--- client/src/components/messenger/components/messageInput/messageInput.js
+++ client/src/components/messenger/components/messageInput/messageInput.js
@@ -1,3 +1,56 @@
-export const MessageInput = () => {
-  return <div>Message Input</div>;
+import "./MessageInput.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
+export const MessageInput = ({ thread }) => {
+  const [message, setMessage] = useState("");
+  const [image, setImage] = useState("");
+
+  const user = useSelector((state) => state.authReducer.user);
+
+  const handleMessage = (event) => {
+    const value = event.target.value;
+    setMessage(value);
+
+    // Notify others that this user is typing
+  };
+
+  const handleKeyDown = (event, imageUpload) => {
+    if (event.key === "Enter") {
+      sendMessage(imageUpload);
+    }
+  };
+
+  const sendMessage = (imageUpload) => {
+    if (message.length < 1 && !imageUpload) {
+      return;
+    }
+    const msg = {
+      type: imageUpload ? "image" : "text",
+      fromUserId: user.id,
+      receiverId: thread.Users.map((user) => user.id),
+      threadId: thread.id,
+      message: imageUpload ? image : message,
+    };
+
+    setMessage("");
+    setImage("");
+
+    // Send Message with Socket
+  };
+
+  return (
+    <div id="input-container">
+      <div id="message-input">
+        <input
+          type="text"
+          placeholder="Enter a Message"
+          onChange={(e) => handleMessage(e)}
+          onKeyDown={(e) => handleKeyDown(e, false)}
+        />
+        <FontAwesomeIcon icon={["far", "smile"]} className="fa-icon" />
+      </div>
+    </div>
+  );
 };
