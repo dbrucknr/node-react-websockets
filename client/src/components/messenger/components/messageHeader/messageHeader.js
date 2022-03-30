a44235fed3f9da8e211a5d117f14f5853ef8d81f
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-30 11:27:50 -0400

    Finalize messageHeader.

--- client/src/components/messenger/components/messageHeader/messageHeader.js
+++ client/src/components/messenger/components/messageHeader/messageHeader.js
@@ -1,3 +1,56 @@
-export const MessageHeader = () => {
-  return <div>Message Header</div>;
+import "./MessageHeader.scss";
+import { Fragment, useState } from "react";
+import { userStatus } from "../../../../utils/helpers";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const MessageHeader = ({ thread }) => {
+  const [showOptions, setShowOptions] = useState(false);
+  const [showAddFriendModal, setShowAddFriendModal] = useState(false);
+  const [showLeaveThreadModal, setShowLeaveThreadModal] = useState(false);
+  const [showDeleteThreadModal, setShowDeleteThreadModal] = useState(false);
+
+  return (
+    <Fragment>
+      <div id="chatter">
+        {thread.Users.map((user) => {
+          return (
+            <div className="chatter-info" key={user.id}>
+              <h3>
+                {user.firstName} {user.lastName}
+              </h3>
+              <div className="chatter-status">
+                <span className={`online-status ${userStatus(user)}`}></span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <FontAwesomeIcon
+        icon={["fas", "ellipsis-v"]}
+        className="fa-icon"
+        onClick={() => setShowOptions(!showOptions)}
+      />
+      {showOptions ? (
+        <div id="settings">
+          <div>
+            <FontAwesomeIcon icon={["fas", "user-plus"]} className="fa-icon" />
+            <p>Add User to Thread</p>
+          </div>
+          {thread.type === "group" ? (
+            <div>
+              <FontAwesomeIcon
+                icon={["fas", "sign-out-alt"]}
+                className="fa-icon"
+              />
+              <p>Leave Thread</p>
+            </div>
+          ) : null}
+          <div>
+            <FontAwesomeIcon icon={["fas", "trash"]} className="fa-icon" />
+            <p>Delete Thread</p>
+          </div>
+        </div>
+      ) : null}
+    </Fragment>
+  );
 };
