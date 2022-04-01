e7b89d831ea9e7f3e1942b613380e8d1f5912f5a
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-04-01 14:20:13 -0400

    refactor authReducer to follow a lookup-table pattern.

--- client/src/store/reducers/auth.js
+++ client/src/store/reducers/auth.js
@@ -1,43 +1,36 @@
-import { LOGIN, REGISTER, LOGOUT, UPDATE_PROFILE } from "../types/index";
-
 const initialState = {
   user: JSON.parse(localStorage.getItem("user")) || {},
   token: localStorage.getItem("token") || "",
   isLoggedIn: !!localStorage.getItem("user"),
 };
 
+const authActionMap = {
+  LOGIN: (state, payload) => ({
+    ...state,
+    user: payload.user,
+    token: payload.token,
+    isLoggedIn: true,
+  }),
+  LOGOUT: (state) => ({
+    ...state,
+    user: {},
+    token: "",
+    isLoggedIn: false,
+  }),
+  REGISTER: (state, payload) => ({
+    ...state,
+    user: payload.user,
+    token: payload.token,
+    isLoggedIn: true,
+  }),
+  UPDATE_PROFILE: (state, payload) => ({
+    ...state,
+    user: payload,
+  }),
+};
+
 export const authReducer = (state = initialState, action) => {
   const { type, payload } = action;
-
-  switch (type) {
-    case LOGIN:
-      return {
-        ...state,
-        user: payload.user,
-        token: payload.token,
-        isLoggedIn: true,
-      };
-    case LOGOUT:
-      return {
-        ...state,
-        user: {},
-        token: "",
-        isLoggedIn: false,
-      };
-    case REGISTER:
-      return {
-        ...state,
-        user: payload.user,
-        token: payload.token,
-        isLoggedIn: true,
-      };
-    case UPDATE_PROFILE:
-      return {
-        ...state,
-        user: payload,
-      };
-    default: {
-      return state;
-    }
-  }
+  const handler = authActionMap[type];
+  return handler ? handler(state, payload) : state;
 };
